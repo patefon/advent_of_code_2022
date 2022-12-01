1939392de7cbe5cad280dec38513f722e6f2c3c2
patefon <dev@headely.com>
2022-12-01 16:34:29 +0400

    refactoring day 1

--- day1/solve.py
+++ day1/solve.py
@@ -1,40 +1,31 @@
 import heapq
 
-PATH = 'data'
-
 def max_calories(p: str):
+  _calories = []
   _max_so_far = _current = 0
   with open(p, 'r') as f:
     while True:
       l =  f.readline()
       if not l:
         _max_so_far = max(_current, _max_so_far)
+        _calories.append(_current)
         break
       if l == '' or l == '\n' or l == '\r\n':
         _max_so_far = max(_current, _max_so_far)
+        _calories.append(_current)
         _current = 0
       else:
         _current += int(str.strip(l))
-  return _max_so_far
+  return _max_so_far, sum(heapq.nlargest(3, _calories))
 
-ans = max_calories(PATH)
-print(f'Answer: {ans}')
+# test
 
-def calories_top_3(p: str):
-  _calories = []
-  _current = 0
-  with open(p, 'r') as f:
-    while True:
-      l =  f.readline()
-      if not l:
-        heapq.heappush(_calories, _current)
-        break
-      if l == '' or l == '\n' or l == '\r\n':
-        heapq.heappush(_calories, _current)
-        _current = 0
-      else:
-        _current += int(str.strip(l))
-  return sum(heapq.nlargest(3, _calories))
+ans1, ans2 = max_calories('data-test')
 
-ans = calories_top_3(PATH)
-print(f'Answer 2: {ans}')
+assert ans1 == 24000
+assert ans2 == 45000
+
+ans1, ans2 = max_calories('data')
+
+print(f'Answer: {ans1}')
+print(f'Answer 2: {ans2}')
